b2ba7c9e09f6968d5d4cc9d788331516b3e842c1
tereza <terezatomic@gmail.com>
2024-11-23 22:57:22 +0100

    Added homepage to package.json, and changed BrowserRouter to HashRouter, in order for it to work with GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Admin from "./components/Admin";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Visitor from "./components/Visitor";
 import LandingPage from "./components/LandingPage";
 
